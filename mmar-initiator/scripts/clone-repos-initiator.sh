57487140812f8432c338d4c0b82f7d30a4362ed2
Gunakar Challa <gunakarchalla@gmail.com>
2026-07-10 16:03:33 +0200

    Add mmar-modeling-client-react to docker setup

--- mmar-initiator/scripts/clone-repos-initiator.sh
+++ mmar-initiator/scripts/clone-repos-initiator.sh
@@ -93,3 +93,5 @@ set_up_repo "https://github.com/gunakarchalla/mmar-sync-server.git" "/usr/src/ap
 set_up_repo "https://github.com/gunakarchalla/mmar-metamodeling-client-react.git" "/usr/src/app/shared/mmar/mmar-metamodeling-client-react"
 
 set_up_repo "https://github.com/gunakarchalla/mmar-vizrep-client-react.git" "/usr/src/app/shared/mmar/mmar-vizrep-client-react"
+
+set_up_repo "https://github.com/gunakarchalla/mmar-modeling-client-react.git" "/usr/src/app/shared/mmar/mmar-modeling-client-react"
---
+++ mmar-modeling-client-react/scripts/start-modeling-client-react.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo "--------------------------------------------------------"
+bash /usr/src/app/npm-installation-modeling-client-react.sh
+echo "--------------------------------------------------------"
+bash /usr/src/app/start-node-modeling-client-react.sh
